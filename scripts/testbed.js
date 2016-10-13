13094b1f4df2dbc2a32a7b8585acabef3caaecdf
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2016-10-13 14:26:35 -0500

    More tile arranging to prove I can make mis-matched sizes

--- scripts/testbed.js
+++ scripts/testbed.js
@@ -20,4 +20,13 @@ var tile3 = document.getElementById("tile3")
 tile3.innerHTML = "<p> Calendar Tile </p>"
 
 var tile4 = document.getElementById("tile4")
-tile4.innerHTML = "<p> To-do List Tile </p>"
+tile4.innerHTML = "<p> Bus Schedule Tile </p>"
+
+var tile5 = document.getElementById("tile5")
+tile5.innerHTML = "<p> News Feed Tile </p>"
+
+var tile6 = document.getElementById("tile6")
+tile6.innerHTML = "<p> Events & To-dos Tile </p>"
+
+var tile7 = document.getElementById("tile7")
+tile7.innerHTML = "<p> Quote of the Day Tile </p>"
