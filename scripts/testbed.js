0849e6461a79aa2dacac6a1ded1d7713bdd4bce1
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2016-10-13 13:41:22 -0500

    Added basic tile layout

--- scripts/testbed.js
+++ scripts/testbed.js
@@ -4,7 +4,7 @@
 // Ex: document.getElementById("p1").innerHTML = "New text!"
 
 
-var body = document.querySelector('body')
-
-body.innerHTML = "<p> I just replaced the text! </p>"+
-"<h2> And I can do many things </h2>"
+// var body = document.querySelector('body')
+//
+// body.innerHTML = "<p> I just replaced the text! </p>"+
+// "<h2> And I can do many things </h2>"
