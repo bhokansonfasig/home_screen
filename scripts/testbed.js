d4a0c130740cc7b556a22a180cbd94e23c1d81b2
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2016-10-13 11:21:51 -0500

    Added testbed.js for testing javascript pieces

---
+++ scripts/testbed.js
@@ -0,0 +1,10 @@
+// To get a single element, use
+// document.getElementById(id)
+// Ex: document.getElementById("myImage").src = "landscape.jpg"
+// Ex: document.getElementById("p1").innerHTML = "New text!"
+
+
+var body = document.querySelector('body')
+
+body.innerHTML = "<p> I just replaced the text! </p>"+
+"<h2> And I can do many things </h2>"
