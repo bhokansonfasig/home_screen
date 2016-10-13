dbf0d8201268b38ef9350a137d39d0872e9a2b1d
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2016-10-13 13:44:11 -0500

    Added example code for changing tile contents

--- scripts/testbed.js
+++ scripts/testbed.js
@@ -8,3 +8,16 @@
 //
 // body.innerHTML = "<p> I just replaced the text! </p>"+
 // "<h2> And I can do many things </h2>"
+
+
+var tile1 = document.getElementById("tile1")
+tile1.innerHTML = "<p> Date & Time Tile </p>"
+
+var tile2 = document.getElementById("tile2")
+tile2.innerHTML = "<p> Weather Tile </p>"
+
+var tile3 = document.getElementById("tile3")
+tile3.innerHTML = "<p> Calendar Tile </p>"
+
+var tile4 = document.getElementById("tile4")
+tile4.innerHTML = "<p> To-do List Tile </p>"
