08de080679ddfb05b8a6067eeb9f163cbe703a41
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2016-10-14 00:09:47 -0500

    Fixed overlap checker and increased push in push_apart

--- scripts/arrange.js
+++ scripts/arrange.js
@@ -5,13 +5,19 @@ var arranged = []
 
 function check_overlap(tile1,tile2) {
   // Checks if tile1 and tile2 are overlapping
-  if (tile1.top<tile2.top && tile1.left<tile2.left &&
-      tile1.top+tile1.height>=tile2.top &&
-      tile1.left+tile2.width>=tile2.left) {
-        return true
+  if (tile1===tile2) {
+    return false
+  }
+  else if ((tile1.left>tile2.left+tile2.width) ||
+      (tile2.left>tile1.left+tile1.width)) {
+        return false
+  }
+  else if ((tile1.top>tile2.top+tile2.height) ||
+      (tile2.top>tile1.top+tile1.height)) {
+        return false
   }
   else {
-    return false
+    return true
   }
 }
 
@@ -32,15 +38,15 @@ function grow(tile,fraction,direction="wh",oversize=false) {
   // Can optionally only expand in one direction
   // By default will not grow tile to larger than its preferred size
   if (direction.includes("w")) {
-    tile.width += Math.floor(tile.settings.size.width*fraction)
+    tile.width += tile.settings.size.width*fraction
     if (tile.width>tile.settings.size.width && !oversize) {
-      tile.width -= Math.floor(tile.settings.size.width*fraction)
+      tile.width -= tile.settings.size.width*fraction
     }
   }
   if (direction.includes("h")) {
-    tile.height += Math.floor(tile.settings.size.height*fraction)
+    tile.height += tile.settings.size.height*fraction
     if (tile.height>tile.settings.size.height && !oversize) {
-      tile.height -= Math.floor(tile.settings.size.height*fraction)
+      tile.height -= tile.settings.size.height*fraction
     }
   }
 }
@@ -51,20 +57,20 @@ function push_apart(tile1,tile2,fraction) {
   var old_positions = [tile1.top,tile1.left,tile2.top,tile2.left]
 
   if (tile1.top<=tile2.top) {
-    tile1.top -= Math.floor(tile1.settings.size.height*fraction/2)
-    tile2.top += Math.floor(tile2.settings.size.height*fraction/2)
+    tile1.top -= tile1.settings.size.height*fraction
+    tile2.top += tile2.settings.size.height*fraction
   }
   else {
-    tile1.top += Math.floor(tile1.settings.size.height*fraction/2)
-    tile2.top -= Math.floor(tile2.settings.size.height*fraction/2)
+    tile1.top += tile1.settings.size.height*fraction
+    tile2.top -= tile2.settings.size.height*fraction
   }
   if (tile1.left<=tile2.left) {
-    tile1.left -= Math.floor(tile1.settings.size.width*fraction/2)
-    tile2.left += Math.floor(tile2.settings.size.width*fraction/2)
+    tile1.left -= tile1.settings.size.width*fraction
+    tile2.left += tile2.settings.size.width*fraction
   }
   else {
-    tile1.left += Math.floor(tile1.settings.size.width*fraction/2)
-    tile2.left -= Math.floor(tile2.settings.size.width*fraction/2)
+    tile1.left += tile1.settings.size.width*fraction
+    tile2.left -= tile2.settings.size.width*fraction
   }
 
   var new_positions = [tile1.top,tile1.left,tile2.top,tile2.left]
@@ -98,11 +104,9 @@ function arrange(tile_objs) {
                  [[],[],[]],
                  [[],[],[]]]
 
-  console.log(tile_objs.length)
   // Set an anchor region for each tile
   for (var tile_i = 0; tile_i < tile_objs.length; tile_i++) {
     var tile = tile_objs[tile_i]
-    console.log(tile.settings)
     for (var i = 0; i < 3; i++) {
       for (var j = 0; j < 3; j++) {
         if (tile.settings.region===regions[i][j]) {
@@ -118,10 +122,10 @@ function arrange(tile_objs) {
     for (var j = 0; j < 3; j++) {
       for (var k = 0; k < anchors[i][j].length; k++) {
         var tile = anchors[i][j][k]
-        tile.width = 0
-        tile.height = 0
-        tile.top = Math.floor((window.innerHeight-50*anchors[i][j].length)*(i+1)/4)+50*k
-        tile.left = Math.floor((window.innerWidth-50*anchors[i][j].length)*(j+1)/4)+50*k
+        tile.width = 10
+        tile.height = 10
+        tile.top = (window.innerHeight-50*anchors[i][j].length)*(i+1)/4+50*k
+        tile.left = (window.innerWidth-50*anchors[i][j].length)*(j+1)/4+50*k
         arranged.push(tile)
       }
     }
@@ -139,7 +143,7 @@ function arrange(tile_objs) {
 
 
   // Grow each tile quickly, then progressively more slowly
-  var fractions = [1/10,1/25,1/50,1/100,1/250,1/500]
+  var fractions = [1/50,1/100,1/250,1/500,1/1000]
   for (var frac_i = 0; frac_i < fractions.length; frac_i++) {
     fraction = fractions[frac_i]
     // Keep looping as long as pushing tiles apart is possible
@@ -156,7 +160,7 @@ function arrange(tile_objs) {
       var over_loops = 0
       while (!overlapping) {
         over_loops++
-        if (over_loops>1) {
+        if (over_loops>50) {
           console.log("Too many overlap loops")
           break
         }
@@ -173,7 +177,6 @@ function arrange(tile_objs) {
               var rewind_tile = arranged[rewind_i]
               grow(rewind_tile,-fraction)
             }
-            console.log("Breaking out of 1")
             break
           }
         }
@@ -186,19 +189,17 @@ function arrange(tile_objs) {
         for (var tile1_i = 0; tile1_i < arranged.length; tile1_i++) {
           var tile1 = arranged[tile1_i]
           if (overlapping || !pushable) {
-            console.log("Breaking out of 2.2")
             break
           }
           for (var tile2_i = 0; tile2_i < arranged.length; tile2_i++) {
             var tile2 = arranged[tile2_i]
             if (check_overlap(tile1,tile2)) {
-              console.log("Something overlaps")
+              console.log("Something overlapping")
               if (!push_apart(tile1,tile2,fraction)) {
-                console.log("Can't push apart")
+                console.log("Not pushable")
                 push_apart(tile1,tile2,-fraction)
                 pushable = false
                 overlapping = true
-                console.log("Breaking out of 2.1")
                 break
               }
             }
@@ -207,7 +208,7 @@ function arrange(tile_objs) {
 
         // Shrink tiles back if necessary
         if (overlapping && !pushable) {
-          console.log("Rewinding growth")
+          console.log("Ungrowing")
           for (var tile_i = 0; tile_i < arranged.length; tile_i++) {
             var tile = arranged[tile_i]
             grow(tile,-fraction)
@@ -225,10 +226,10 @@ function arrange(tile_objs) {
 function setup(id,tile_obj) {
   var tile = document.getElementById(id)
   tile.innerHTML = tile_obj.html
-  tile.style.top = tile_obj.top.toString()+"px"
-  tile.style.left = tile_obj.left.toString()+"px"
-  tile.style.width = tile_obj.width.toString()+"px"
-  tile.style.height = tile_obj.height.toString()+"px"
+  tile.style.top = Math.floor(tile_obj.top).toString()+"px"
+  tile.style.left = Math.floor(tile_obj.left).toString()+"px"
+  tile.style.width = Math.floor(tile_obj.width).toString()+"px"
+  tile.style.height = Math.floor(tile_obj.height).toString()+"px"
   tile.style.color = tile_obj.color
 }
 
