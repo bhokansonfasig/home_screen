632306f03312eba5aad89451f459d2a9a2c9b1a5
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2016-10-16 11:34:33 -0500

    Implemented updating of tile htmls and tested with datetime tile

--- tiles/datetime/tile.js
+++ tiles/datetime/tile.js
@@ -7,9 +7,34 @@ datetime.settings.size = JSON.parse(size)
 datetime.settings.region = JSON.parse(region)
 datetime.settings.color = JSON.parse(color)
 
-datetime.html =
-"<h2> Thursday, October 16th <\h2>"+
-"<h1> 4:52 pm </h1>"
+
+datetime.update_interval = 1000
+datetime.update = function(tile) {
+  var dt = new Date()
+  var day = dt.getDay()
+  var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+  var month = dt.getMonth()
+  var months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+  var hour = dt.getHours()
+  var ampm = "am"
+  day = days[day]
+  month = months[month]
+  if (hour==0) {
+    hour = 12
+  }
+  else if (hour==12) {
+    ampm = "pm"
+  }
+  else if (hour>12) {
+    ampm = "pm"
+    hour = hour - 12
+  }
+  tile.html =
+  '<p style="font-size:25px"> '+day+"<br>"+month+" "+dt.getDate()+" </p>"+
+  '<p style="font-size:40px"> '+hour.toString()+":"+dt.getMinutes()+" "+ampm+" </p>"
+}
+
+datetime.html = "<p> datetime </p>"
 
 
 tiles.datetime = datetime
