94b61fa73e8169f2e864a3fbf4178209de69459b
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2016-11-04 23:43:24 -0500

    Bus tile now works as it should. Only direct routes allowed at this point (not going to change this in the foreseeable future)

--- tiles/busses/tile.js
+++ tiles/busses/tile.js
@@ -11,6 +11,8 @@ busses.api = {}
 busses.api.key = JSON.parse(key)
 busses.api.places = JSON.parse(places)
 
+busses.routes = []
+
 
 busses.update_interval = 3600000
 busses.update = function(tile) {
@@ -31,18 +33,18 @@ busses.update = function(tile) {
   tile.transit_info = []
 
   for (var i = 0; i < destinations.length; i++) {
-    request_bus_info(tile,destinations[i],i)
+    request_bus_info(tile,destinations[i],destinations.length)
   }
 
 }
 
 
-function request_bus_info(tile,destination,slot_num) {
+function request_bus_info(tile,destination,dest_length) {
   var xmlHttp = new XMLHttpRequest()
   xmlHttp.onreadystatechange = function() {
     if (xmlHttp.readyState===4 && xmlHttp.status===200)
       parseGMresponse(JSON.parse(xmlHttp.responseText),
-                      tile,destination,slot_num)
+                      tile,destination,dest_length)
     else if (xmlHttp.readyState===4) {
       tile.innerHTML = '<div class="busses" id="busses_error">'+
                        'Error getting transit info; Google Maps may be down</div>'
@@ -52,48 +54,65 @@ function request_bus_info(tile,destination,slot_num) {
   var home = busses.api.places.home.split(' ').join('+')
   var dest = busses.api.places[destination].split(' ').join('+')
   var url = "https://maps.googleapis.com/maps/api/directions/json?origin="+home+
-            "&destination="+dest+"&mode=transit&key="+key
+            "&destination="+dest+"&mode=transit&alternatives=true&key="+key
   xmlHttp.open("GET",url,true)
   xmlHttp.send(null)
 }
 
 
-function parseGMresponse(response,tile,destination,slot_num) {
+function parseGMresponse(response,tile,destination,dest_length) {
+  console.log(response)
+
   if (!tile.element.innerHTML.includes("bus_slot_0")) {
     set_HTML_base(tile)
+    busses.routes = []
   }
-  var slot = document.getElementById('bus_slot_'+slot_num)
+  // var slot = document.getElementById('bus_slot_'+slot_num)
+  busses.routes.push([destination])
+
   if (response.status==="OK") {
-    for (var i = 0; i < response.routes[0].legs[0].steps.length; i++) {
-      var step = response.routes[0].legs[0].steps[i]
-      if (step.travel_mode==="TRANSIT") {
-        set_slot_HTML(slot,step,destination)
-        break
+    for (var i = 0; i < response.routes.length; i++) {
+      // Get only direct routes
+      var transit_index = -1
+      for (var j = 0; j < response.routes[i].legs[0].steps.length; j++) {
+        if (response.routes[i].legs[0].steps[j].travel_mode==="TRANSIT") {
+          if (transit_index===-1) {
+            transit_index = j
+          }
+          else {
+            transit_index = -1
+            break
+          }
+        }
       }
+      if (transit_index!==-1) {
+        busses.routes[busses.routes.length-1].push(
+          response.routes[i].legs[0].steps[transit_index])
+      }
+
+      // for (var j = 0; j < response.routes[i].legs[0].steps.length; j++) {
+      //   var step = response.routes[i].legs[0].steps[j]
+      //   if (step.travel_mode==="TRANSIT") {
+      //     busses.routes[busses.routes.length-1].push(step)
+      //     break
+      //   }
+      // }
     }
   }
   else {
-    console.log(response.status)
-    slot.innerHTML = "<p>Error: "+response.status+"</p>"
+    // slot.innerHTML = "<p>Error: "+response.status+"</p>"
   }
-}
 
-
-function set_slot_HTML(slot,directions,destination) {
-  console.log(directions.transit_details)
-  var bus_name = directions.transit_details.line.short_name
-  var depart = directions.transit_details.departure_time.text
-  var arrive = directions.transit_details.arrival_time.text
-  slot.innerHTML +=
-    '<div class="schedule_desc"> '+bus_name+' to '+destination+' </div>'+
-    '<div class="schedule_depart"> '+depart+' </div>'+
-    '<div class="schedule_arrive"> '+arrive+' </div>'
+  if (busses.routes.length===dest_length) {
+    set_routes_HTML(tile)
+  }
 }
 
 
 function set_HTML_base(tile) {
   tile.element.innerHTML =
       '<div class="busses" id="title_bus">Bus<hr color="black"></div>'+
+      '<div class="busses" id="title_dest">Destination<hr color="black"></div>'+
       '<div class="busses" id="title_depart">Depart<hr color="black"></div>'+
       '<div class="busses" id="title_arrive">Arrive<hr color="black"></div>'
 
@@ -102,6 +121,44 @@ function set_HTML_base(tile) {
     tile.element.innerHTML +=
       '<div class="bus_schedule" id="bus_slot_'+i+'"'+
       ' style="top:'+top_pos+'%"> </div>'
+  }
+}
+
+
+function set_slot_HTML(slot,directions,destination) {
+  var bus_name = directions.transit_details.line.short_name
+  if (Number(bus_name)!=NaN) {
+    bus_name = Number(bus_name)
+  }
+  var depart = directions.transit_details.departure_time.text
+  var arrive = directions.transit_details.arrival_time.text
+  slot.innerHTML +=
+    '<div class="schedule_bus"> '+bus_name+' </div>'+
+    '<div class="schedule_dest"> '+destination+' </div>'+
+    '<div class="schedule_depart"> '+depart+' </div>'+
+    '<div class="schedule_arrive"> '+arrive+' </div>'
+}
+
+
+function set_routes_HTML(tile) {
+  var depart_times = []
+  for (var i = 0; i < busses.routes.length; i++) {
+    for (var j = 1; j < busses.routes[i].length; j++) {
+      depart_times.push([i,j,
+        busses.routes[i][j].transit_details.departure_time.value])
+    }
+  }
+  // console.log(depart_times)
+  depart_times.sort(
+    function(a,b) {
+      return a[2]-b[2]
+    }
+  )
+  for (var i = 0; i < tile.settings.num_slots; i++) {
+    var slot = document.getElementById('bus_slot_'+i)
+    var dest = busses.routes[depart_times[i][0]][0]
+    var dirs = busses.routes[depart_times[i][0]][depart_times[i][1]]
+    set_slot_HTML(slot,dirs,dest)
   }
 }
 
