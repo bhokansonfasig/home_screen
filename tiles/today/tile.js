4a62c99aef0fbe1e7c89585b2edd215e8f9c3cb4
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2016-11-13 12:01:38 -0600

    Added support for multiple calendars in today tile

--- tiles/today/tile.js
+++ tiles/today/tile.js
@@ -14,28 +14,33 @@ today.update_interval = 3600000
 today.update = function(tile) {
   tile.element.innerHTML = "<h1> Today </h1>"
 
+  tile.calendars = []
+  var xmlHttp = {}
   for (var cal_name in today.calendar_links) {
-    var xmlHttp = new XMLHttpRequest()
-    xmlHttp.onreadystatechange = function() {
-      if (xmlHttp.readyState===4 && xmlHttp.status===200)
-        parseCALresponse(xmlHttp.responseText,cal_name)
-      else if (xmlHttp.readyState===4) {
-        tile.innerHTML = 'Error getting calendar info'
+    (function(cal_name) {
+      xmlHttp[cal_name] = new XMLHttpRequest()
+      xmlHttp[cal_name].onreadystatechange = function() {
+        if (xmlHttp[cal_name].readyState===4 && xmlHttp[cal_name].status===200) {
+          parseCALresponse(tile,xmlHttp[cal_name].responseText,cal_name)
+        }
+        else if (xmlHttp[cal_name].readyState===4) {
+          console.log('Error getting calendar info')
+        }
       }
-    }
-    var url = today.calendar_links[cal_name]
-    xmlHttp.open("GET",url,true)
-    xmlHttp.send(null)
+      var url = today.calendar_links[cal_name]
+      xmlHttp[cal_name].open("GET",url,true)
+      xmlHttp[cal_name].send(null)
+    })(cal_name)
   }
 }
 
 
-function parseCALresponse(response,cal_name) {
+function parseCALresponse(tile,response,cal_name) {
   var events = parse_ical(response)
-  today.calendars.push([cal_name,events])
-  if (today.calendars.length===Object.keys(today.calendar_links).length) {
+  tile.calendars.push([cal_name,events])
+  if (tile.calendars.length===Object.keys(tile.calendar_links).length) {
     console.log("Finished parsing all calendars")
-    console.log(today.calendars)
+    console.log(tile.calendars)
   }
 }
 
