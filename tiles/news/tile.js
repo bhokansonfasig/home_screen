81fb1e5e2c02ed0aa62494afeb295f31ed7b0f9a
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2016-11-13 19:04:11 -0600

    Set up for proper documenting of sources and their stories

--- tiles/news/tile.js
+++ tiles/news/tile.js
@@ -25,8 +25,13 @@ news.update = function(tile) {
        if (xmlHttp[source].readyState===4 && xmlHttp[source].status===200)
          parseFEEDresponse(tile,xmlHttp[source].responseText,source)
        else if (xmlHttp[source].readyState===4) {
-         tile.element.innerHTML = '<div class="news" id="news_error">'+
-                                  'Error getting news; sources may be down</div>'
+         console.log("Error getting info from news source "+source)
+         tile.total_sources = tile.total_sources - 1
+         if (tile.total_sources===0) {
+           tile.element.innerHTML = '<div class="news" id="news_error">'+
+                                    'Error getting news; sources may be down</div>'
+         }
+
        }
       }
       var url = tile.news_sources[source]
@@ -39,16 +44,25 @@ news.update = function(tile) {
 
 function parseFEEDresponse(tile,response,source) {
   var url = tile.news_sources[source]
-  stories = []
 
   if (url.indexOf("reddit")>-1) {
-    stories += parse_reddit_feed(response)
+    tile.sources.push([source,parse_reddit_feed(response)])
   }
   else if (url.indexOf("bbc")>-1) {
-    stories += parse_bbc_feed(response)
+    tile.sources.push([source,parse_bbc_feed(response)])
   }
   else {
     console.log("Not sure how to pull news stories from "+url)
+    tile.total_sources = tile.total_sources - 1
+    if (tile.total_sources===0) {
+      tile.element.innerHTML = '<div class="news" id="news_error">'+
+                               'Error getting news; sources may be down</div>'
+    }
+  }
+
+  if (tile.sources.length===tile.total_sources) {
+    console.log("Finished gathering news")
+    console.log(tile.sources)
   }
 }
 
