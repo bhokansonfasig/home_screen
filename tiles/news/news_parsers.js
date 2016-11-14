aef29d133e5020ffd276650ced281e6c5de76f44
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2016-11-13 18:53:47 -0600

    Starting process of getting news stories from websites

---
+++ tiles/news/news_parsers.js
@@ -0,0 +1,14 @@
+function parse_reddit_feed(dump) {
+  stories = []
+
+
+  return stories
+}
+
+
+function parse_bbc_feed(dump) {
+  stories = []
+
+
+  return stories
+}
--- tiles/news/tile.js
+++ tiles/news/tile.js
@@ -7,10 +7,49 @@ news.settings.size = JSON.parse(size)
 news.settings.region = JSON.parse(region)
 news.settings.color = JSON.parse(color)
 
+news.news_sources = JSON.parse(news_sources)
+news.total_sources = Object.keys(news.news_sources).length
+news.sources = []
 
 news.update_interval = 3600000
 news.update = function(tile) {
-  tile.element.innerHTML ="<h1> News </h1>"
+  tile.element.innerHTML = '<div class="news" id="news_error">'+
+                           'Gathering news...</div>'
+
+  tile.calendars = []
+  var xmlHttp = {}
+  for (var source in tile.news_sources) {
+    (function(source) {
+      xmlHttp[source] = new XMLHttpRequest()
+      xmlHttp[source].onreadystatechange = function() {
+       if (xmlHttp[source].readyState===4 && xmlHttp[source].status===200)
+         parseFEEDresponse(tile,xmlHttp[source].responseText,source)
+       else if (xmlHttp[source].readyState===4) {
+         tile.element.innerHTML = '<div class="news" id="news_error">'+
+                                  'Error getting news; sources may be down</div>'
+       }
+      }
+      var url = tile.news_sources[source]
+      xmlHttp[source].open("GET",url,true)
+      xmlHttp[source].send(null)
+    })(source)
+  }
+}
+
+
+function parseFEEDresponse(tile,response,source) {
+  var url = tile.news_sources[source]
+  stories = []
+
+  if (url.indexOf("reddit")>-1) {
+    stories += parse_reddit_feed(response)
+  }
+  else if (url.indexOf("bbc")>-1) {
+    stories += parse_bbc_feed(response)
+  }
+  else {
+    console.log("Not sure how to pull news stories from "+url)
+  }
 }
 
 
